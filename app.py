aa89b90df2176afc5caa33df531be199b5824f8c
T Bharadwaj <121448283+TeluguBharadwaj@users.noreply.github.com>
2023-12-18 20:59:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ Created on Mon Dec 18 20:11:21 2023
 
 @author: telug
 """
-!pip install streamlit_drawable_canvas
+# Install streamlit_drawable_canvas
+subprocess.run(["pip", "install", "streamlit_drawable_canvas"])
 
 from streamlit_drawable_canvas import st_canvas
 import streamlit as st
